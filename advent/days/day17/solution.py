a7799c11ecb82cfc55f33cca02308315542d087d
Rüdiger Ludwig <ruediger.ludwig@posteo.de>
2024-12-17 20:42:04 +0100

    day 17 finished

--- advent/days/day17/solution.py
+++ advent/days/day17/solution.py
@@ -1,21 +1,11 @@
 from __future__ import annotations
 
 from collections.abc import Iterator
-from dataclasses import dataclass, field
-from enum import Enum
+from dataclasses import dataclass
+
+from advent.common.helper import as_two_tuple
 
 day_num = 17
-
-
-class Instruction(Enum):
-    Adv = 0
-    Bxl = 1
-    Bst = 2
-    Jnz = 3
-    Bxc = 4
-    Out = 5
-    Bdv = 6
-    Cdv = 7
 
 
 @dataclass(slots=True)
@@ -23,20 +13,21 @@ class Program:
     registers: list[int]
     code: list[int]
     ip: int = 0
-    out: list[int] = field(default_factory=list)
+
+    @staticmethod
+    def second_part(data: str, divide: str) -> str:
+        _, payload = data.split(":", maxsplit=2)
+        return payload.strip()
 
     @classmethod
     def parse(cls, lines: Iterator[str]) -> Program:
-        _, reg_a = next(lines).split(":")
-        reg_a = int(reg_a.strip())
-        _, reg_b = next(lines).split(":")
-        reg_b = int(reg_b.strip())
-        _, reg_c = next(lines).split(":")
-        reg_c = int(reg_c.strip())
+        reg_a = int(Program.second_part(next(lines), ":"))
+        reg_b = int(Program.second_part(next(lines), ":"))
+        reg_c = int(Program.second_part(next(lines), ":"))
         _ = next(lines)
-        _, prog = next(lines).split(":")
-        program = [int(i.strip()) for i in prog.split(",")]
-        return Program([reg_a, reg_b, reg_c], program)
+        code_str = Program.second_part(next(lines), ":")
+        code = [int(i) for i in code_str.split(",")]
+        return Program([reg_a, reg_b, reg_c], code)
 
     def combo(self, value: int) -> int:
         match value:
@@ -51,54 +42,95 @@ class Program:
             case _:
                 raise Exception("Unknown Operand")
 
-    def step(self):
-        match self.code[self.ip]:
-            case 0:  # adv
-                self.registers[0] //= 2 ** self.combo(self.code[self.ip + 1])
-                self.ip += 2
-            case 1:  # bxl
-                self.registers[1] ^= self.code[self.ip + 1]
-                self.ip += 2
-            case 2:  # bst
-                self.registers[1] = self.combo(self.code[self.ip + 1]) % 8
-                self.ip += 2
-            case 3:  # bst
-                if self.registers[0]:
-                    self.ip = self.code[self.ip + 1]
-                else:
-                    self.ip += 2
-            case 4:  # bxc
-                self.registers[1] ^= self.registers[2]
-                self.ip += 2
-            case 5:  # out
-                self.out.append(self.combo(self.code[self.ip + 1]) % 8)
-                self.ip += 2
-            case 6:  # bdv
-                self.registers[1] = self.registers[0] // 2 ** self.combo(
-                    self.code[self.ip + 1]
-                )
-                self.ip += 2
-            case 7:  # cdv
-                self.registers[2] = self.registers[0] // 2 ** self.combo(
-                    self.code[self.ip + 1]
-                )
-                self.ip += 2
-            case _:
-                raise Exception("Illegal inst")
-
-    def run(self) -> str:
+    def run(self) -> Iterator[int]:
         while self.ip < len(self.code):
-            self.step()
-        return ",".join(str(o) for o in self.out)
+            if (out := self.run_once()) is not None:
+                yield out
 
-    def is_copy(self) -> bool:
-        return self.code == self.out
+    def run_once(self) -> int | None:
+        while self.ip < len(self.code):
+            match self.code[self.ip], self.code[self.ip + 1]:
+                case 0, v:  # adv
+                    self.registers[0] >>= self.combo(v)
+                    self.ip += 2
+                case 1, v:  # bxl
+                    self.registers[1] ^= v
+                    self.ip += 2
+                case 2, v:  # bst
+                    self.registers[1] = self.combo(v) % 8
+                    self.ip += 2
+                case 3, v:  # jnz
+                    if self.registers[0]:
+                        self.ip = v
+                    else:
+                        self.ip += 2
+                case 4, _:  # bxc
+                    self.registers[1] ^= self.registers[2]
+                    self.ip += 2
+                case 5, v:  # out
+                    out = self.combo(v) % 8
+                    self.ip += 2
+                    return out
+                case 6, v:  # bdv
+                    self.registers[1] = self.registers[0] >> self.combo(v)
+                    self.ip += 2
+                case 7, v:  # cdv
+                    self.registers[2] = self.registers[0] >> self.combo(v)
+                    self.ip += 2
+                case c, _:
+                    raise Exception(f"Illegal code: {c}")
+        return None
+
+    def test_numbers(self, num: list[int], value: int, bits: int) -> int | None:
+        if not num:
+            return value
+
+        to_hit = num[0]
+        value <<= bits
+        for check in range(2**bits):
+            self.registers[0] = value + check
+            self.ip = 0
+            out = self.run_once()
+            if out == to_hit:
+                result = self.test_numbers(num[1:], value + check, bits)
+                if result is not None:
+                    return result
+        return None
+
+    def check_assumptions(self):
+        # we have an even number of code instructions
+        assert len(self.code) % 2 == 0
+
+        # there is exactly one output
+        assert 1 == sum(1 for a, _ in as_two_tuple(self.code) if a == 5)
+
+        # register a is changed only once
+        assert 1 == sum(1 for a, _ in as_two_tuple(self.code) if a == 0)
+        assert 1 == sum(1 for a, b in as_two_tuple(self.code) if a == 0 and b > 0)
+
+        # the last instruction is the only jump and it jumps back to 0
+        assert 1 == sum(1 for a, _ in as_two_tuple(self.code) if a == 3)
+        assert self.code[-2] == 3
+        assert self.code[-1] == 0
+
+    def run_backwards(self) -> int:
+        self.check_assumptions()
+
+        bits = sum(b for a, b in as_two_tuple(self.code) if a == 0)
+
+        test = list(reversed(self.code))
+        result = self.test_numbers(test, 0, bits)
+        if result is None:
+            raise Exception("No valid number found")
+
+        return result
 
 
 def part1(lines: Iterator[str]) -> str:
     prog = Program.parse(lines)
-    return prog.run()
+    return ",".join(str(o) for o in prog.run())
 
 
 def part2(lines: Iterator[str]) -> int:
-    return None
+    prog = Program.parse(lines)
+    return prog.run_backwards()
--- advent/days/day17/test_solution.py
+++ advent/days/day17/test_solution.py
@@ -4,53 +4,53 @@ from .solution import Program, day_num, part1, part2
 
 
 def test_part1():
-    lines = input.read_lines(day_num, "input.txt")
+    lines = input.read_lines(day_num, "example01.txt")
     expected = "4,6,3,5,6,3,5,2,1,0"
     result = part1(lines)
     assert result == expected
 
 
 def test_part2():
-    lines = input.read_lines(day_num, "example01.txt")
-    expected = None
+    lines = input.read_lines(day_num, "example03.txt")
+    expected = 117440
     result = part2(lines)
+    assert result == expected
+
+
+def test_backwards():
+    lines = input.read_lines(day_num, "example03.txt")
+    expected = 117440
+    prog = Program.parse(lines)
+    result = prog.run_backwards()
     assert result == expected
 
 
 def test_small1():
     prog = Program([0, 0, 9], [2, 6])
-    prog.step()
+    prog.run_once()
     assert prog.registers[1] == 1
 
 
 def test_small2():
     prog = Program([10, 0, 0], [5, 0, 5, 1, 5, 4])
-    prog.run()
-    assert prog.out == [0, 1, 2]
+    out = list(prog.run())
+    assert out == [0, 1, 2]
 
 
 def test_small3():
     prog = Program([2024, 0, 0], [0, 1, 5, 4, 3, 0])
-    prog.run()
-    assert prog.out == [4, 2, 5, 6, 7, 7, 7, 7, 3, 1, 0]
+    out = list(prog.run())
+    assert out == [4, 2, 5, 6, 7, 7, 7, 7, 3, 1, 0]
     assert prog.registers[0] == 0
 
 
 def test_small4():
     prog = Program([0, 29, 0], [1, 7])
-    prog.run()
+    list(prog.run())
     assert prog.registers[1] == 26
 
 
 def test_small5():
     prog = Program([0, 2024, 43690], [4, 0])
-    prog.run()
+    list(prog.run())
     assert prog.registers[1] == 44354
-
-
-def test_dummy():
-    lines = input.read_lines(day_num, "example02.txt")
-    prog = Program.parse(lines)
-    prog.registers[0] = 117440
-    prog.run()
-    assert prog.is_copy()
